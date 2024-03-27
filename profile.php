98be5adbe0f3a07366e98594e4c86f9ede401f54
Sifflet_blanc <76435000+Sifflet-Blanc@users.noreply.github.com>
2024-03-27 18:51:57 +0100

    improve profile page

--- profile.php
+++ profile.php
@@ -22,57 +22,62 @@ if(isset($_GET['password']) AND isset($_GET['user'])){
 				<link rel="stylesheet" type="text/css" href="css/profile.css" media="all" />
 			</head>
 			<body>
-				<div id="profilePres">
-					<div>
-						<div id="profileLeft">
-							<img id="profilePicture" src="img/<?php echo $User['IdAccount'].'/'.$User['ProfilePictureFile'];?>">
+				<div class="centreur">
+					<div id="profilePres">
+						<div>
+							<div id="profileTop">
+								<img id="profilePicture" src="img/<?php echo $User['IdAccount'].'/'.$User['ProfilePictureFile'];?>">
+							</div>
+							<div id="profileBottom">
+							</div>
 						</div>
-						<div id="profileRight">
-							<?php 
-								$Picture_tab = $bdd->query('SELECT ImgFile FROM Image WHERE IdAccount='.$User['IdAccount']);
-								if($Picture=$Picture_tab->fetch()){
-									do{
-										echo '<img src="img/'.$User['IdAccount'].'/'.$Picture["ImgFile"].'">';
-									}while($Picture=$Picture_tab->fetch());
-								}
-								else{
-									echo '<div id="nopicture"></div>';
-								}
-							?>
+					</div>
+					<div class="right" id="pictureContener">
+						<?php 
+							$Picture_tab = $bdd->query('SELECT ImgFile FROM Image WHERE IdAccount='.$User['IdAccount']);
+							if($Picture=$Picture_tab->fetch()){
+								do{
+									echo '<img src="img/'.$User['IdAccount'].'/'.$Picture["ImgFile"].'">';
+								}while($Picture=$Picture_tab->fetch());
+							}
+							else{
+								echo '<div id="nopicture"></div>';
+							}
+						?>
+					</div>
+					<div class="right" id="modifierHide">
+						<h2>Informations :</h2>
+						<label for="pseudo">Pseudo : </label><br>
+						<label for="name">Nom :</label><br>
+						<label for="firstname">Prenom :</label><br>
+						<div>
+							<h3>Adresse</h3>
+							<label for="password">Pays : </label><br>
+							<label for="password">Ville : </label><br>
+							<label for="password">Rue : </label><br>
+							<label for="password">Numero : </label><br>
 						</div>
-				</div>
-				<div id="modifierHide">
-					<h2>Informations :</h2>
-					<label for="password">Pseudo : 
-					<label for="password">Nom :
-					<label for="password">Prenom :
-					<div>
-						<h3>Adresse</h3>
-						<label for="password">Pays : 
-						<label for="password">Ville : 
-						<label for="password">Rue : 
-						<label for="password">Numero : 
+						<div>
+							<h3>Descriptif</h3>
+							<label for="password">Sexe : </label><br>
+							<label for="password">Date de naissance : </label><br>
+							<label for="password">Espece : </label><br>
+							<label for="password">Jauge humanitée : </label><br>
+							<label for="password">Taille : </label><br>
+							<label for="password">Poids : </label><br>
+							<label for="password">Couleur des yeux : </label><br>
+						</div>
+						Changer le mot de passe
+						<form method="post" action="new_password.php">
+							<label for="password">Mot de passe :</label><br>
+							<input type="password" name="password" id="password" maxlength="255" minlength="6"/><br>
+							<label for="password">Nouveau mot de passe :</label><br>
+							<input type="password" name="password" id="password" maxlength="255" minlength="6"/><br>
+							<label for="password_confirm">Confirmation du mot de passe :</label><br>
+							<input type="password" name="password_confirm" id="password_confirm" maxlength="255" minlength="6"/><br>
+							<input type="submit" name="Ajouter" value="Changer">
+						</form>
 					</div>
-					<div>
-						<h3>Descriptif</h3>
-						<label for="password">Sexe : 
-						<label for="password">Date de naissance : 
-						<label for="password">Espece : 
-						<label for="password">Jauge humanitée : 
-						<label for="password">Taille : 
-						<label for="password">Poids : 
-						<label for="password">Couleur des yeux : 
-					</div>
-					Changer le mot de passe
-					<form method="post" action="new_password.php">
-						<label for="password">Mot de passe :</label><br>
-						<input type="password" name="password" id="password" maxlength="255" minlength="6"/><br>
-						<label for="password">Nouveau mot de passe :</label><br>
-						<input type="password" name="password" id="password" maxlength="255" minlength="6"/><br>
-						<label for="password_confirm">Confirmation du mot de passe :</label><br>
-						<input type="password" name="password_confirm" id="password_confirm" maxlength="255" minlength="6"/><br>
-						<input type="submit" name="Ajouter" value="Changer">
-					</form>
 				</div>
 			</body>
 		</html>
