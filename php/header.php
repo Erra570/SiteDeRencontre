d593351490ba5ee960cea7a9abcf7863772178ec
Rubenso <Rubens.tueux@cy-tech.fr>
2024-04-12 01:47:02 +0200

    abonnement sur la page abonné

--- php/header.php
+++ php/header.php
@@ -34,6 +34,7 @@ if (isset($_SESSION['user']) AND isset($_SESSION['password'])){
             <div class="bandeau">
                     <div class ="logo"><a href="index.php"><img src="img/logo.png", width = 100%></a></div>
                     <a href="index.php">Accueil</a></li>
+                    <a href="subscription.php">Abonnements</a></li>
                     <a href="recherche.php">Recherche</a></li>
                     <a href="messagerie.php">Messagerie</a></li>
                     <a href="profil.php"><?php echo $user; ?></a></li>
