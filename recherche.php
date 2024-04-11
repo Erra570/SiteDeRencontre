c2c88cbe86150c789215e71350641acdc8908455
Sifflet_blanc <76435000+Sifflet-Blanc@users.noreply.github.com>
2024-04-11 21:16:14 +0200

    Avancée Recherche

--- recherche.php
+++ recherche.php
@@ -14,28 +14,54 @@ if (isset($_SESSION['password']) && isset($_SESSION['user'])) {
     $User_tab = $bdd->prepare('SELECT * FROM Account WHERE Pseudo=:user AND Password=:password');
     $User_tab->execute(array('user' => $user, 'password' => $password));
     
-    if ($User = $User_tab->fetch()) {
-        include("php/header.php");
-        include("php/head.html");
-        
-        if (isset($_POST['search_user'])) {
-            $search_user = htmlspecialchars($_POST['search_user']);
-        
-            $sql = "SELECT * FROM Account WHERE Pseudo LIKE '%$search_user%' OR Name LIKE '%$search_user%' OR FirstName LIKE '%$search_user%'";
-        
-            $result = $bdd->query($sql);
-				
-            if ($result->rowCount() > 0) {
-                echo "<h2>Résultats de la recherche :</h2>";
-                echo "<ul>";
-                while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                    echo "<li>Nom d'utilisateur: " . $row["Pseudo"]. "</li>";
-                }
-                echo "</ul>";
-            } else {
-                echo "Aucun résultat trouvé.";
-            }
-        }
+    if ($User = $User_tab->fetch()) { ?>
+
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <?php include("php/header.php"); ?>
+                <link rel="stylesheet" type="text/css" href="css/recherche.css" media="all" />
+            </head>
+            <body>
+                <?php include("php/head.html"); ?>
+                <div id="contener">
+                    <div>
+                        <form method="post" class="form">
+                            <label for="search_user">Rechercher un utilisateur :</label>
+                            <input type="text" name="search_user" id="search_user">
+                            <input type="submit" value="Rechercher">
+                        </form>
+                        <?php
+                        if (isset($_POST['search_user'])) {
+                            $search_user = htmlspecialchars($_POST['search_user']);
+                        
+                            $sql = "SELECT * FROM Account WHERE Pseudo LIKE '%$search_user%' OR Name LIKE '%$search_user%' OR FirstName LIKE '%$search_user%'";
+                        
+                            $result = $bdd->query($sql);
+                				
+                            if ($result->rowCount() > 0) { ?>
+                                <h2>Résultats de la recherche :</h2>
+                                <a id="profils" href="profilPublic.php?currentUser=<?php echo $row['Pseudo']; ?>">
+                                    <?php
+                                    while ($row = $result->fetch(PDO::FETCH_ASSOC)) { ?>
+                                        <div class="profil">
+                                            <img class="profilPicture" src="img/<?php echo $row['IdAccount']."/".$row['ProfilPictureFile'];?>">
+                                            <div class="pseudo"><?php echo $row['Pseudo']; ?></div>
+                                            <div class="pseudo"><?php echo $row['FirstName']; ?></div>
+                                            <div class="pseudo"><?php echo $row['Name']; ?></div>
+                                        </div>
+                                    <?php } ?>
+                                </a>
+                            <?php
+                            } else {
+                                echo "Aucun résultat trouvé.";
+                            }
+                        } ?>
+                    </div>
+                </div>
+            </body>
+        </html>
+    <?php
     } else {
         echo "Erreur d'authentification.";
     }
@@ -43,16 +69,3 @@ if (isset($_SESSION['password']) && isset($_SESSION['user'])) {
     echo "Veuillez vous connecter.";
 }
 ?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<link rel="stylesheet" type="text/css" href="css/recherche.css" media="all" />
-	</head>
-	<body>
-		<form method="post" class="form">
-    		<label for="search_user">Rechercher un utilisateur :</label>
-    		<input type="text" name="search_user" id="search_user">
-    		<input type="submit" value="Rechercher">
-		</form>
-	</body>
-</html>
